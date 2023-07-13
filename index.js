e9c2ae8a14f5ef5d7bc9bce4428372157c7936b3
Moebirdie <93432701+Moebirdie@users.noreply.github.com>
2023-07-12 21:35:20 -0400

    on roll with tests - saving progress before implementing parent class render method

--- index.js
+++ index.js
@@ -2,4 +2,9 @@ const fs = require('fs');
 //const inquirer = require('./node_modules/inquirer');
 const userinput = require('./lib/userinputs');
 
+
 const newAnswers = userinput.askQuestions();
+
+// const renderShape = () => {
+//     if 
+// }
